a718784ae8b76afc8d0624326234414b7809d76f
JaanDev <67502867+JaanDev@users.noreply.github.com>
2024-02-16 20:07:16 +0300

    update

--- src/patches.cpp
+++ src/patches.cpp
@@ -1,16 +1,21 @@
 #include <Geode/Geode.hpp>
 
+#include "Geode/modify/CCKeyboardDispatcher.hpp"
 #include "utils.hpp"
 
 using namespace geode::prelude;
 
-$execute {
-    SetConsoleOutputCP(65001); // utf8
-    
+void patchStrings() {
     static std::vector<std::string> strings;
-    
-	// locationsFile = gdlutils::loadJson((Mod::get()->getResourcesDir() / "ru_ru_locations.json").string());
-    auto langFile = gdlutils::loadJson((Mod::get()->getResourcesDir() / "ru_lang.json").string());
+
+    for (auto p : Mod::get()->getPatches()) {
+        if (p->isEnabled())
+            p->disable();
+    }
+
+    // locationsFile = gdlutils::loadJson((Mod::get()->getResourcesDir() / "ru_ru_locations.json").string());
+    auto langFile = gdlutils::loadJson((ghc::filesystem::current_path() / "ru_lang.json").string());
+    // auto langFile = gdlutils::loadJson((Mod::get()->getResourcesDir() / "ru_lang.json").string());
 
     strings.clear();
     strings.reserve(langFile.size());
@@ -21,9 +26,9 @@ $execute {
     for (const auto& pair : langFile.items()) {
         if (!patchFile.contains(pair.key()))
             continue;
-    
+
         strings.push_back(pair.value());
-    
+
         for (const auto addr : patchFile[pair.key()]) {
             const char* str = strings[strings.size() - 1].c_str();
             Mod::get()->patch((void*)(base::get() + addr), ByteVector((uint8_t*)&str, (uint8_t*)&str + 4));
@@ -31,16 +36,16 @@ $execute {
     }
 #elif defined(GEODE_IS_ANDROID)
     // auto patchFile = gdlutils::loadJson((Mod::get()->getResourcesDir() / "gdl_patches-android.json").string());
-    
+
     // for (const auto& pair : langFile.items()) {
     //     if (!patchFile.contains(pair.key()))
     //         continue;
-    
+
     //     strings.push_back(pair.value());
 
     //     const char* str = strings[strings.size() - 1].c_str();
     //     auto array = patchFile[pair.key()].get<nlohmann::json::array_t>();
- 
+
     //     for(const auto& addr : array[0].get<nlohmann::json::array_t>()) {
     //         Mod::get()->patch((void*)(base::get() + addr.get<uintptr_t>()), ByteVector((uint8_t*)&str, (uint8_t*)&str + 4));
     //     }
@@ -51,3 +56,21 @@ $execute {
     // }
 #endif
 }
+
+$execute {
+    SetConsoleOutputCP(65001); // utf8
+
+    patchStrings();
+}
+
+class $modify(CCKeyboardDispatcher) {
+    bool dispatchKeyboardMSG(enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
+        if (isKeyDown && key == enumKeyCodes::KEY_P) {
+            patchStrings();
+            Notification::create("GDL: Updated strings", NotificationIcon::Success)->show();
+            return true;
+        }
+
+        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
+    }
+};
