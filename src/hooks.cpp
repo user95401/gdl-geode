f449c2aa4f23436a5062234cb05a8f04f578aaa6
JaanDev <67502867+JaanDev@users.noreply.github.com>
2024-04-08 17:23:31 +0300

    another important hook

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -142,20 +142,28 @@ class $modify(CCTextureCache) {
 void (__thiscall* gd_string_assign_o)(void* self, char* src, size_t len);
 void gd_string_assign_hk(void* self, char* src, size_t len) {
 /*     
-        Зачем нужны эти хуки:
-        В некоторых случаях в гд, функция GDString::assign вызывается в заданной длинной,
+        Зачем нужен этот хук:
+        В некоторых случаях в гд функция GDString::assign вызывается в заданной длинной,
         которая ограничивает длинну строки, но переведенная строка может быть длиннее чем оригинальная.
         Было бы намного сложнее пропатчить все длины строк, учитывая то, что не везде указывается длина строки.
-        В общем намного легче хукнуть эту функция и установить новую длину строки.
+        В общем, намного легче хукнуть эту функцию и установить новую длину строки.
 
         Why this is required:
-        In some places of gd you can see GDString::assign called with fixed length
-        which limits strings to the length it was in the game but the translated strings may be longer than the original ones.
-        It is also pretty difficult to patch the length as it is not used in all calls and i would somehow need to find places to patch
-        So it is easier to hook it and just set it to the string length instead of the fixed length
+        In some places of gd you can see that GDString::assign is called with fixed length
+        which limits a string to the length it was in the game but a translated string may be longer than the original one.
+        It is also pretty difficult to patch the length as it is not used in all calls and i would somehow need to find places to patch.
+        So it is easier to hook it and just set it to the string length instead of the fixed length.
 */
 	gd_string_assign_o(self, src, strlen(src));
 }
+
+void (__thiscall* gd_string_append_o)(void* self, char* src, size_t len);
+void gd_string_append_hk(void* self, char* src, size_t len) {
+    // Same as for gd::string::assign
+
+	gd_string_append_o(self, src, strlen(src));
+}
+
 
 $execute {
     auto addr = GetProcAddress(GetModuleHandleA("libcocos2d.dll"), "?FNTConfigLoadFile@cocos2d@@YAPAVCCBMFontConfiguration@1@PBD@Z");
@@ -169,5 +177,12 @@ $execute {
     auto res2 = Mod::get()->hook((void*)(base::get() + GD_STR_ASSIGN_ADDR), gd_string_assign_hk, "gd::string::assign", tulip::hook::TulipConvention::Thiscall).err();
     if (res2 != std::nullopt) {
         log::error("Failed to hook gd::string::assign because of: {}", res2);
+    }
+    
+    constexpr auto GD_STR_APPEND_ADDR = 0x21DE0;
+    gd_string_append_o = reinterpret_cast<void (__thiscall*)(void*, char*, size_t)>(base::get() + GD_STR_APPEND_ADDR);
+    auto res3 = Mod::get()->hook((void*)(base::get() + GD_STR_APPEND_ADDR), gd_string_append_hk, "gd::string::append", tulip::hook::TulipConvention::Thiscall).err();
+    if (res3 != std::nullopt) {
+        log::error("Failed to hook gd::string::append because of: {}", res2);
     }
 }
