be4ddf8789daec92da3bff9efc07bf4d14c8c464
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-01-07 00:04:29 +0300

    reformat

--- src/patches.cpp
+++ src/patches.cpp
@@ -39,18 +39,20 @@ void patches::patchStrings() {
     patches::fixCyrillicP();
 
 #ifdef GEODE_IS_WINDOWS64
-        bool res3;
-        res3 = gdl::patchCString(base::get() + 0x320CB5, "Настройки");
-//     log::debug("{}", res3);
-//     res3 = gdl::patchCString(base::get() + 0x350598, "Привет, мир 2!");
-//     log::debug("{}", res3);
-//     res3 = gdl::patchCString(base::get() + 0x3505F1, "Привет, мир 3!");
-//     log::debug("{}", res3);
+    bool res3;
+    res3 = gdl::patchCString(base::get() + 0x320CB5, "Настройки");
 
-//     bool res2;
-//     res2 = gdl::patchStdStringRel("hi", 0x31561F, 0x31562F, 0x315638, {0x315641, 0x315648, 0x31564B, 0x315652, 0x315656, 0x31565C, 0x31565F, 0x315666, 0x31566A});
-//     // res2 = gdl::patchStdStringRel("This is a very very long string1!This is a very very long string2!This is a very very long string3!This is a very very long string4!This is a very very long string5!", 0x31561F, 0x31562F, 0x315638, {0x315641, 0x315648, 0x31564B, 0x315652, 0x315656, 0x31565C, 0x31565F, 0x315666, 0x31566A});
-//     log::debug("res {}", res2);
+    ///////////// OLD /////////////
+    // log::debug("{}", res3);
+    // res3 = gdl::patchCString(base::get() + 0x350598, "Привет, мир 2!");
+    // log::debug("{}", res3);
+    // res3 = gdl::patchCString(base::get() + 0x3505F1, "Привет, мир 3!");
+    // log::debug("{}", res3);
+
+    // bool res2;
+    // res2 = gdl::patchStdStringRel("hi", 0x31561F, 0x31562F, 0x315638, {0x315641, 0x315648, 0x31564B, 0x315652, 0x315656, 0x31565C, 0x31565F, 0x315666, 0x31566A});
+    // // res2 = gdl::patchStdStringRel("This is a very very long string1!This is a very very long string2!This is a very very long string3!This is a very very long string4!This is a very very long string5!", 0x31561F, 0x31562F, 0x315638, {0x315641, 0x315648, 0x31564B, 0x315652, 0x315656, 0x31565C, 0x31565F, 0x315666, 0x31566A});
+    // log::debug("res {}", res2);
 
     // bool res2;
     // res2 = gdl::patchStdString2("Hello world! This is a long enough string!", {{base::get() + 0x31561F, 0x4F}}, base::get() + 0x31562A); // quit text
@@ -59,6 +61,7 @@ void patches::patchStrings() {
     // log::debug("res {}", res2);
     // res2 = gdl::patchStdString2("Hello!", {{base::get() + 0x67032, 0x2a}}, base::get() + 0x67052); // "Ease Out" which is inlined
     // log::debug("res {}", res2);
+    ///////////////////////////////
 
     // bool res;
     // res = gdl::patchCString(base::get() + 0x35C17A, "Привет GDL!"); // yay it works
